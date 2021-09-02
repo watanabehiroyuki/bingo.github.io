3a13e809767ec8c2d42ee4409f84ebc565b87e82
watanabehiroyuki <88261502+watanabehiroyuki@users.noreply.github.com>
2021-09-03 01:08:38 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+'use strict';
+{
+  function createColumn(col){
+    const source = [];
+    for (let i = 0; i < 15;i++){
+      source[i] = i + 1 + 15 * col;
+    }
+  
+    const column = [];
+    for(let i = 0; i < 5;i++){
+      column[i] = source.splice(Math.floor(Math.random() * source.length),1)[0];
+    }
+
+    return column;
+
+  }
+
+  const columns = [];
+  columns[0] = createColumn(0);
+  columns[1] = createColumn(1);
+  columns[2] = createColumn(2);
+  columns[3] = createColumn(3);
+  columns[4] = createColumn(4);
+  columns[2][2] = 'FREE';
+
+  // console.table(columns);
+
+  const bingo = [];
+  for (let row = 0; row < 5;row++) {
+    bingo[row] = [];
+    for (let col = 0; col < 5;col++) {
+      bingo[row][col] = columns[col][row];
+    }
+  }
+  console.table(bingo);
+
+  for (let row = 0; row < 5; row++) {
+    const tr = document.createElement('tr');
+    for (let col = 0; col < 5; col++) {
+      const td = document.createElement('td');
+      td.textContent = bingo[row][col];
+      tr.appendChild(td);
+    }
+    document.querySelector('tbody').appendChild(tr);
+  }
+}
